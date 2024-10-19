a31e13cbfc6bfc0035712adfccdded373a873e75
rizaru22 <rizaru.2.2@gmail.com>
2024-10-19 12:22:10 +0700

    penambahan title

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
+<?php
+  if(isset($_GET['title'])){
+    $title=$_GET['title'];
+  }else{
+    $title="Aplikasi Perpustakaan";
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Blank Page</title>
+  <title><?=strtoupper($title) ?></title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -70,7 +77,7 @@
             <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
             <li class="nav-item">
-              <a href="index.php?page=dashboard" class="nav-link">
+              <a href="index.php?page=dashboard&title=dashboard" class="nav-link <?php if($title==='dashboard') echo 'active'; ?>">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>
                   Dashboard
@@ -78,7 +85,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a href="index.php?page=siswa" class="nav-link">
+              <a href="index.php?page=siswa&title=siswa" class="nav-link <?php if($title==='siswa') echo 'active'; ?>">
                 <i class="nav-icon fas fa-users"></i>
                 <p>
                   Siswa
@@ -126,9 +133,6 @@
         } else {
           include "view/dashboard.php";
         }
-
-
-
         ?>
 
       </section>
